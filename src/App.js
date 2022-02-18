83cd3ee6acabfb2b379cefd928d0285445d16d82
Pranav Ajay <pranavajay2002@gmail.com>
2022-02-18 16:24:07 +0530

    Created highlight outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Stack } from "@mui/material";
 import NavBar from "./components/NavBar";
 import Box from '@mui/material/Box';
+import Highlight from "./components/Highlight";
 
 function App() {
   return (
 <div>
   <NavBar />
+  <Highlight />
 
 </div>
     
---
+++ src/components/Highlight.jsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+function Highlight() {
+  return (
+    <Box 
+    height={'60vh'}
+    bgcolor='red'
+    ></Box>
+  )
+}
+
+export default Highlight
